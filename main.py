6c54ef597a8db945aaa335e8c34c2e22099b8307
Shashwat Pandey <pandeyshashwat422@gmail.com>
2026-07-15 17:30:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ app = FastAPI()
 # CORS must be added immediately after creating the app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-quiz-generator-1fi9.onrender.com"],   # or ["*"] while developing
+    allow_origins=["https://ai-quiz-generator-1fi9.onrender.com"],
+    allow_origins=["https://ai-code-explainer-6y77.onrender.com"],
+    allow_origins=["https://ai-notes-summarizer-gwej.onrender.com"],
+    allow_origins=["https://ai-content-writer-1.onrender.com"],
+    allow_origins=["https://ai-travel-planner-b67c.onrender.com"],
+    allow_origins=["https://ai-notes-summarizer-gwej.onrender.com"],
+    allow_origins=["https://ai-study-notes-generator-b2rh.onrender.com"],
+    allow_origins=["https://ai-assigment-helper.onrender.com"],
+    allow_origins=["https://ai-doubt-solver-9pvd.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -18,7 +26,7 @@ app.add_middleware(
 class PromptRequest(BaseModel):
     prompt: str
 
-@app.post("/chat")
+@app.post("/")
 def chat(request: PromptRequest):
     answer = ask_groq(request.prompt)
     return {"response": answer}
