456c8ab03b02518cf035ab6774d55153c210c1ce
Shashwat Pandey <pandeyshashwat422@gmail.com>
2026-07-15 17:43:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from groq_service import ask_groq
 
 app = FastAPI()
 
-# CORS must be added immediately after creating the app
+# CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ai-quiz-generator-1fi9.onrender.com"],
-    allow_origins=["https://ai-code-explainer-6y77.onrender.com"],
-    allow_origins=["https://ai-notes-summarizer-gwej.onrender.com"],
-    allow_origins=["https://ai-content-writer-1.onrender.com"],
-    allow_origins=["https://ai-travel-planner-b67c.onrender.com"],
-    allow_origins=["https://ai-notes-summarizer-gwej.onrender.com"],
-    allow_origins=["https://ai-study-notes-generator-b2rh.onrender.com"],
-    allow_origins=["https://ai-assigment-helper.onrender.com"],
-    allow_origins=["https://ai-doubt-solver-9pvd.onrender.com"],
+    allow_origins=[
+        "https://ai-quiz-generator-1fi9.onrender.com",
+        "https://ai-code-explainer-6y77.onrender.com",
+        "https://ai-notes-summarizer-gwej.onrender.com",
+        "https://ai-content-writer-1.onrender.com",
+        "https://ai-travel-planner-b67c.onrender.com",
+        "https://ai-study-notes-generator-b2rh.onrender.com",
+        "https://ai-assigment-helper.onrender.com",
+        "https://ai-doubt-solver-9pvd.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
